2a9bffcf85fb33e10f31d496bcb0af3c5beeecbc
Anders Törnqvist <anders.tornqvist@gmail.com>
2017-09-15 09:57:08 +0200

    Use web pack to deliver typescript to the browser

---
+++ Makefile
@@ -0,0 +1,12 @@
+.PHONY: help
+help:
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | \
+	awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+.PHONY: start
+start: ## starts the dev environment
+	@modd -f support/modd.conf
+
+.PHONY: tree
+tree: ## lists the projects file structure
+	tree -I node_modules
---
+++ support/webpack.dev.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        filename: "./public/bundle.js"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        loaders: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+        ]
+    }
+
+    // Other options...
+};
