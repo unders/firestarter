122166ff6aac0770a9b9a93ea34361fe7f46a1b5
Anders Törnqvist <anders.tornqvist@gmail.com>
2017-09-28 16:18:00 +0200

    Test that App updates web browser on state change.

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,12 +1,25 @@
 import {Client, Headers} from "./client/client";
+import { CSS as css} from './data/css';
 import {App} from "./app/app";
 import {CommentService} from "./service/comment";
 import {adjustSnap} from "./test/helper";
 import {CommentComponent} from "./component/comment";
 
 class Page {
+    readonly app: App;
+    constructor(app: App) {
+        this.app = app;
+    }
     showForm() {
+        const root = document.body.querySelector("#funcbox-comment") as Element;
+        expect(root).not.toBeNull();
+        const el = root.querySelector("[data-showForm]") as HTMLElement;
+        expect(el).not.toBeNull();
+        el.click();
 
+        const form = this.app.getState().commentFormWidget;
+        expect(form.placeholder.klass).toEqual(css.hide);
+        expect(form.form.klass).toEqual(css.show);
     }
 }
 
@@ -16,9 +29,15 @@ describe("App", () => {
     const client = new Client("", new Headers(), 0);
     const commentService = new CommentService(client, []);
     const app = new App(commentService);
+    const page = new Page(app);
 
     test("#render()", () => {
         app.render();
         expect(adjustSnap(document.body.innerHTML)).toMatchSnapshot();
+    });
+
+    test("show form", () => {
+       page.showForm();
+       expect(adjustSnap(document.body.innerHTML)).toMatchSnapshot();
     });
 });
