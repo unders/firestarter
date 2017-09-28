2a0275d50b944e00b683bd9c050ebaddad310f1e
Anders Törnqvist <anders.tornqvist@gmail.com>
2017-09-28 10:44:50 +0200

    Add sleep helper

--- src/test/helper.ts
+++ src/test/helper.ts
@@ -2,3 +2,9 @@ export const adjustSnap = (html: string): string => {
     let pattern = new RegExp('<!--_hyper: -\d+;-->');
     return html.replace(/_hyper:\s*-?\d+/g, '');
 };
+
+
+// await sleep(1000) // sleep for 1 second.
+export const sleep = (ms: number): Promise<any> => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
